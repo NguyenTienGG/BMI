4735a19bf1e55bc98bba59a1e3da3ed7c9de93b5
ADMIN <tiendong2kk2@gmail.com>
2024-07-13 14:10:12 +0700

    done

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.bmicalculator
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bmicalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.weightPicker.minValue = 30
+        binding.weightPicker.maxValue = 200
+
+        binding.heightPicker.minValue = 100
+        binding.heightPicker.maxValue = 250
+
+        binding.weightPicker.setOnValueChangedListener { _, _, _ ->
+            calculateBMI()
+        }
+
+        binding.heightPicker.setOnValueChangedListener { _, _, _ ->
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI() {
+        val weight = binding.weightPicker.value
+        val height = binding.heightPicker.value
+        val doubleHeight = height / 100.0
+        val bmi = weight / (doubleHeight * doubleHeight)
+        binding.resultsTV.text = String.format("Your BMI: %.2f", bmi)
+        binding.healthyTV.text = String.format("Considered: %s", heathyMessage(bmi))
+    }
+
+    private fun heathyMessage(bmi: Double): String {
+        return when {
+            bmi < 18.5 -> "Underweight"
+            bmi < 25 -> "Healthy"
+            bmi < 30 -> "Overweight"
+            else -> "Obese"
+        }
+    }
+}
